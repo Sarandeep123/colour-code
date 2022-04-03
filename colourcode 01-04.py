95ace1ce6ba0b8563d21075b16939a706baac8c5
Sarandeep123 <102315666+Sarandeep123@users.noreply.github.com>
2022-04-03 09:55:57 +0530

    Add files via upload

---
+++ colourcode 01-04.py
@@ -0,0 +1,17 @@
+c=input("Enter the colour code:")
+if c=='Y':
+    print(c,"-->Yellow")
+elif c=='R':
+    print(c,"-->Red")
+elif c=='G':
+    print(c,"-->Green")
+elif c=='B':
+    print(c,"-->Blue")
+elif c=='V':
+    print(c,"-->Voilet")
+elif c=='O':
+    print(c,"-->Orange")
+elif c=='I':
+    print(c,"-->Indigo")
+else:
+    print("Invalid colour code")
